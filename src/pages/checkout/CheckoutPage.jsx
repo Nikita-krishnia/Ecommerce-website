2e6dee84635f2fec5437e011067792ae7f260852
Nikita <nikitakrishnia08@gmail.com>
2025-12-13 10:02:17 +0530

    separate CartItemDetail and DeliveryDate into different components

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -13,14 +13,23 @@ export function CheckoutPage({ cart }) {
 
     useEffect(() => {
         const fetchCheckoutData=async()=>{
-            let response=await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
+            const response=await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
                 setDeliveryOptions(response.data);
+        };
+            fetchCheckoutData();
+        
+        }, []);
 
-            response=await axios.get('./api/payment-summary');
+    useEffect(()=>{
+        const fetchPaymentSummary=async()=>{
+            const  response=await axios.get('./api/payment-summary');
                 setPaymentSummary(response.data);
             };
-            fetchCheckoutData();
-        }, []);
+            fetchPaymentSummary();
+        },[cart]);
+
+
+
     return (
         <>
             <title>Checkout</title>
--- src/pages/orders/OrderPage.jsx
+++ src/pages/orders/OrderPage.jsx
@@ -11,11 +11,12 @@ export function OrderPage({ cart }) {
     const [orders, setOrders] = useState([]);
 
     useEffect(() => {
-        axios.get('/api/orders?expand=products')
-            .then((response) => {
+        const fetchOrderPageData=async()=>{
+        const response = await axios.get('/api/orders?expand=products');
                 setOrders(response.data);
-            });
-    }, []);
+        };
+        fetchOrderPageData();
+}, []);
     return (
         <>
             <title>Orders</title>
