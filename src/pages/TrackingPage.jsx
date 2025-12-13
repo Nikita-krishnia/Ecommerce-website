fd8aa6b71e850ff1e24da56713e9338368e90a0a
Nikita <nikitakrishnia08@gmail.com>
2025-12-13 19:25:17 +0530

    Maintain tackingPage calculation of delivery date

--- src/pages/TrackingPage.jsx
+++ src/pages/TrackingPage.jsx
@@ -1,12 +1,52 @@
 import './TrackingPage.css';
 import { Header } from '../components/Header';
-export function TrackingPage() {
+import { useParams } from 'react-router';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+
+export function TrackingPage({ cart }) {
+
+    const { orderId, productId } = useParams();
+    const [order, setOrder] = useState(null);
+
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        };
+
+        fetchTrackingData();
+    }, [orderId]);
+
+    if (!order) {
+        return null;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    });
+
+
+    const totalDeliveryTimeMs = orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+    const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+
+    let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+    if (deliveryPercent > 100) {
+        deliveryPercent = 100;
+    }
+
+    const isPreparing = deliveryPercent < 33;
+    const isShipped = deliveryPercent >= 33 && deliveryPercent < 100;
+    const isDelivered = deliveryPercent === 100;
+
+
     return (
         <>
             <title>Tracking</title>
             <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
-            <Header />
-``
+            <Header cart={cart} />
+            ``
             <div className="tracking-page">
                 <div className="order-tracking">
                     <a className="back-to-orders-link link-primary" href="/orders">
@@ -14,33 +54,38 @@ export function TrackingPage() {
                     </a>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        {deliveryPercent >= 100 ? 'Delivery on' : 'Arriving on'}
+                        {dayjs(orderProduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.Quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
-                        <div className="progress-label">
+                        <div className={`progress-label ${isPreparing && 'current-status'}`}>
                             Preparing
                         </div>
-                        <div className="progress-label current-status">
+                        <div className={`progress-label ${isShipped && 'current-status'}`}>
                             Shipped
                         </div>
-                        <div className="progress-label">
+                        <div className={`progress-label ${isDelivered && 'current-status'}`}>                            
                             Delivered
                         </div>
                     </div>
 
                     <div className="progress-bar-container">
-                        <div className="progress-bar"></div>
+
+                        <div className="progress-bar" style={{
+                            width: `${deliveryPercent}`
+                        }}></div>
                     </div>
                 </div>
             </div>
--- src/pages/checkout/CheckoutHeader.jsx
+++ src/pages/checkout/CheckoutHeader.jsx
@@ -3,7 +3,13 @@ import { Link } from 'react-router';
 import CheckoutLockIcon from '../../assets/images/icons/checkout-lock-icon.png';
 import Logo from '../../assets/images/logo.png';
 import MobileLogo from '../../assets/images/mobile-logo.png';
-export function CheckoutHeader() {
+
+
+export function CheckoutHeader({cart=[]}) {
+    let totalQuantity=0;
+    cart.forEach((cartItem) => {
+        totalQuantity+=cartItem.quantity;
+    });
     return (
         <div className="checkout-header">
             <div className="header-content">
@@ -16,7 +22,8 @@ export function CheckoutHeader() {
 
                 <div className="checkout-header-middle-section">
                     Checkout (<a className="return-to-home-link"
-                        href="/">3 items</a>)
+                        // href="/">3 items</a>)
+                        href='/'>{totalQuantity} items </a>)
                 </div>
 
                 <div className="checkout-header-right-section">
