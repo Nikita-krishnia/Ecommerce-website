2c34854d5cce7e0a065ee621429ba3625b891367
Nikita <nikitakrishnia08@gmail.com>
2025-12-16 09:55:11 +0530

    make search button active

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,31 @@ import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
 import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
-import { NavLink } from 'react-router';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import { useState } from 'react';
 
 
-export function Header({cart=[]}) {
+export function Header({ cart = [] }) {
+    const navigate = useNavigate();
+    const [searchParams] = useSearchParams();
+    const searchText = searchParams.get('search');
 
-    let totalQuantity=0;
-    cart.forEach((cartItem)=>{
-        totalQuantity+=cartItem.quantity;
+    // || '' is a shortcut. It means if searchText does not exist
+    // it will use a default value of ''.
+    const [search, setSearch] = useState(searchText || '');
+
+
+    const updateSearchInput = (event) => {
+        setSearch(event.target.value);
+    };
+
+    const searchProducts = () => {
+        navigate(`/?search=${search}`);
+    };
+
+    let totalQuantity = 0;
+    cart.forEach((cartItem) => {
+        totalQuantity += cartItem.quantity;
     })
 
     return (
@@ -21,14 +38,16 @@ export function Header({cart=[]}) {
                     <img className="logo"
                         src={LogoWhite} />
                     <img className="mobile-logo"
-                        src={MobileLogoWhite}/>
+                        src={MobileLogoWhite} />
                 </NavLink>
             </div>
 
             <div className="middle-section">
-                <input className="search-bar" type="text" placeholder="Search" />
+                <input className="search-bar" type="text" placeholder="Search"
+                    value={search} onChange={updateSearchInput} />
 
-                <button className="search-button">
+                <button className="search-button"
+                    onClick={searchProducts}>
                     <img className="search-icon" src={SearchIcon} />
                 </button>
             </div>
