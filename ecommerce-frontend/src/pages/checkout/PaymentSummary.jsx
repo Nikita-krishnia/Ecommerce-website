dad8be77d6e7fde617e281c3912fc802af7402f2
Nikita <145992983+Nikita-krishnia@users.noreply.github.com>
2026-01-16 21:40:03 +0530

    Update API call in PaymentSummary component

--- ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-frontend/src/pages/checkout/PaymentSummary.jsx
@@ -5,7 +5,7 @@ import { formatMoney } from '../../utils/money';
 export function PaymentSummary({paymentSummary,loadCart}) {
     const navigate=useNavigate();
     const createOrder=async()=>{
-        await axios.post('/api/orders');
+        await api.post('/api/orders');
         await loadCart();
         navigate('/orders');
     };
