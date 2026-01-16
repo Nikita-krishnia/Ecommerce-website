34f382d4c9e65db23e6073cfcf9e34f6c19cab9e
Nikita <145992983+Nikita-krishnia@users.noreply.github.com>
2026-01-16 21:37:58 +0530

    Update API call in Product test to use api.post

--- ecommerce-frontend/src/pages/home/Product.test.jsx
+++ ecommerce-frontend/src/pages/home/Product.test.jsx
@@ -82,7 +82,7 @@ describe('Product component', () => {
     const addToCartButton = screen.getByTestId('add-to-cart-button');
     await user.click(addToCartButton);
 
-    expect(axios.post).toHaveBeenCalledWith(
+    expect(api.post).toHaveBeenCalledWith(
       '/api/cart-items',
       {
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
