69c02fdeb653d9f40f47a995d432a445127f9faa
Nikita <145992983+Nikita-krishnia@users.noreply.github.com>
2026-01-16 21:39:09 +0530

    Update HomePage.jsx

--- ecommerce-frontend/src/pages/home/HomePage.jsx
+++ ecommerce-frontend/src/pages/home/HomePage.jsx
@@ -14,7 +14,7 @@ export function HomePage({cart,loadCart}) {
          
          const getHomeData=async ()=>{
             const urlPath = search ? `/api/products?search=${search}` : '/api/products';
-            const response = await axios.get(urlPath);
+            const response = await api.get(urlPath);
             setProducts(response.data);
          };
     getHomeData();
