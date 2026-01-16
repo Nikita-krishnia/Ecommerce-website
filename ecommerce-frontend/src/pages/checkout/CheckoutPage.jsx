3755a15994507d183a11f45d9eddc3c83e9d107d
Nikita <145992983+Nikita-krishnia@users.noreply.github.com>
2026-01-16 19:54:10 +0530

    correcting after deploy on render

--- ecommerce-frontend/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-frontend/src/pages/checkout/CheckoutPage.jsx
@@ -1,48 +1,54 @@
-import './checkoutHeader.css';
-import './CheckoutPage.css'
+import './CheckoutHeader.css';
+import './CheckoutPage.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { CheckoutHeader } from './CheckoutHeader';
 import { OrderSummary } from './OrderSummary';
 import { PaymentSummary } from './PaymentSummary';
 
+export function CheckoutPage({ cart, loadCart }) {
+  const [deliveryOptions, setDeliveryOptions] = useState([]);
+  const [paymentSummary, setPaymentSummary] = useState(null);
 
-export function CheckoutPage({ cart,loadCart}) {
-    const [deliveryOptions, setDeliveryOptions] = useState([]);
-    const [paymentSummary, setPaymentSummary] = useState(null);
+  useEffect(() => {
+    const fetchCheckoutData = async () => {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/delivery-options?expand=estimatedDeliveryTime`
+      );
+      setDeliveryOptions(response.data);
+    };
+    fetchCheckoutData();
+  }, []);
 
-    useEffect(() => {
-        const fetchCheckoutData=async()=>{
-            const response=await axios.get('/api/delivery-options?expand=estimatedDeliveryTime');
-                setDeliveryOptions(response.data);
-        };
-            fetchCheckoutData();
-        
-        }, []);
+  useEffect(() => {
+    const fetchPaymentSummary = async () => {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/api/payment-summary`
+      );
+      setPaymentSummary(response.data);
+    };
+    fetchPaymentSummary();
+  }, [cart]);
 
-    useEffect(()=>{
-        const fetchPaymentSummary=async()=>{
-            const  response=await axios.get('./api/payment-summary');
-                setPaymentSummary(response.data);
-            };
-            fetchPaymentSummary();
-        },[cart]);
+  return (
+    <>
+      <title>Checkout</title>
+      <CheckoutHeader />
+      <div className="checkout-page">
+        <div className="page-title">Review your order</div>
 
-
-
-    return (
-        <>
-            <title>Checkout</title>
-            <CheckoutHeader />
-            <div className="checkout-page">
-                <div className="page-title">Review your order</div>
-
-                <div className="checkout-grid">
-                    <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart} />
-
-                    <PaymentSummary paymentSummary={paymentSummary} loadCart={loadCart} />
-                </div>
-            </div>
-        </>
-    );
+        <div className="checkout-grid">
+          <OrderSummary
+            cart={cart}
+            deliveryOptions={deliveryOptions}
+            loadCart={loadCart}
+          />
+          <PaymentSummary
+            paymentSummary={paymentSummary}
+            loadCart={loadCart}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
