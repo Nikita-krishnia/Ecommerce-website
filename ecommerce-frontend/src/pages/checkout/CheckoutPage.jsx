c066bace33d3177c9eec272a4dd1c37989cb4340
Nikita <145992983+Nikita-krishnia@users.noreply.github.com>
2026-01-16 21:42:04 +0530

    Refactor API call to use api instance

--- ecommerce-frontend/src/pages/checkout/CheckoutPage.jsx
+++ ecommerce-frontend/src/pages/checkout/CheckoutPage.jsx
@@ -12,7 +12,7 @@ export function CheckoutPage({ cart, loadCart }) {
 
   useEffect(() => {
     const fetchCheckoutData = async () => {
-      const response = await axios.get(
+      const response = await api.get(
         `${import.meta.env.VITE_API_URL}/api/delivery-options?expand=estimatedDeliveryTime`
       );
       setDeliveryOptions(response.data);
